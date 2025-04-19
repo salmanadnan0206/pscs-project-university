9fafb4261bf786a500dc8a5fddc93818da7dba68
salmanadnan0206 <salmanadnan0206@gmail.com>
2025-04-19 20:14:32 +0500

    Fixed run-windows.ps1

--- run-windows.ps1
+++ run-windows.ps1
@@ -1,10 +1,18 @@
-# run-windows.ps1
-
 # 1) Ensure VcXsrv/Xming is running (Disable access control)
-# 2) Tell Docker the display endpoint
+# 2) Set the display endpoint for Docker
 $Env:DISPLAY = "host.docker.internal:0.0"
 
-# 3) Run the container
+# 3) Check if the Docker image exists locally
+$imageExists = docker image inspect pscs-app:latest -f '{{.Id}}' -ErrorAction SilentlyContinue
+
+if (-not $imageExists) {
+    Write-Host "Docker image 'pscs-app:latest' not found locally. Building the image..."
+    docker build -t pscs-app:latest .
+} else {
+    Write-Host "Docker image 'pscs-app:latest' found locally."
+}
+
+# 4) Run the container
 docker run -it --rm `
   -e DISPLAY=$Env:DISPLAY `
   pscs-app
